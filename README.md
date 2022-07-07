afeaf6b193ac390a5ecf79c1250527357e243b7b
Orlando <orlando.groppo@gmail.com>
2022-07-07 19:09:17 +0200

    MUI version

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # frontend-coding
 
+Setup the project:
+
+Make sure you install all the dependencies (currently yarn, but you can opt-out) and start the solution in dev mode.
+
+There is a simple homepage with links to the tasks below.
+
 First Task:
 
 - in the "feed" page, show a centered column of posts (use https://jsonplaceholder.typicode.com/ to get the data) which are simple boxes with the title and body properties returned
@@ -8,13 +14,18 @@ First Task:
 
 Second Task:
 
-- create a "vault" page, showing a responsive grid of suqare pictures (use https://jsonplaceholder.typicode.com/ to get the data) which are simple thumbnails
+- create a "vault" page, showing a responsive grid of square pictures (use https://jsonplaceholder.typicode.com/ to get the data) which are simple thumbnails
 - when clicking on a thumbnail, the fullscreen image opens
+
+Requirements:
+
+- You shuld use MUI5 components https://mui.com/material-ui/
+- You can create your own component if needed, style it with the default `styled` from MUI without installing other libraries
 
 Bonus points:
 
+- SSR considerations
 - Type the responses from the API calls
 - create meaningful tags in the head of each page
-- creative css styling is welcome
-- add the favicon stealing it from fanvue.com
+- add the favicon stealing it from fanvue.com ;)
 - a11y considerations
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,72 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import NextLink from "next/link";
+import { Paper, Box, Typography, Link, Grid } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title></title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fanvue's Frontend coding challenge</h1>
-
-        <div className={styles.grid}>
-          <a href="/feed" className={styles.card}>
-            <h2>Go to Feed page</h2>
-            <p>And start the first task</p>
-          </a>
-          <a href="/vault" className={styles.card}>
-            <h2>Go to Vault page</h2>
-            <p>And start the second task</p>
-          </a>
-        </div>
+      <main>
+        <Box textAlign={"center"} my={8}>
+          <Typography component="h1" variant="h3">
+            Fanvue&apos;s Frontend coding challenge
+          </Typography>
+        </Box>
+        <Grid container spacing={1}>
+          <Grid
+            item
+            xs={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <NextLink href="/feed" passHref>
+              <Link>
+                <Paper
+                  sx={{
+                    textAlign: "center",
+                    border: "1px solid grey",
+                    borderRadius: 5,
+                    p: 3,
+                  }}
+                  elevation={3}
+                >
+                  <h2>Go to Feed page</h2>
+                  <p>And start the first task</p>
+                </Paper>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <NextLink href="/vault" passHref>
+              <Link>
+                <Paper
+                  sx={{
+                    textAlign: "center",
+                    border: "1px solid grey",
+                    borderRadius: 5,
+                    p: 3,
+                  }}
+                  elevation={3}
+                >
+                  <h2>Go to Vault page</h2>
+                  <p>And start the second task</p>
+                </Paper>
+              </Link>
+            </NextLink>
+          </Grid>
+        </Grid>
       </main>
     </div>
   );
